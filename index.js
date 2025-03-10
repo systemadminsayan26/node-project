a78d510c1f2715bf05e58d6d9615ced2dbee93eb
openweb <sayan@pit.com>
2025-03-10 11:15:14 +0000

    update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const config = yaml.load(fs.readFileSync("config.yml", "utf8"));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello from sayan and Agnimita !" });
+  res.json({ message: "Hello from Pitangent to lambda !" });
 });
 
 const PORT = process.env.PORT || config.server.port;
